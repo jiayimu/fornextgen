cdf7ff92c4e12528163f9da674f37e536358c9e9
jiayimu <mjylove005@gmail.com>
2016-10-04 22:39:22 -0400

    First Initial

---
+++ footer.php
@@ -0,0 +1,6 @@
+        <!--<?php require_once('footer.php');?>-->
+        <!--below is footer-->
+    </div>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="zh-cn">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>美中下一代教育基金会</title>
+
+    <link href="bootstrapSource/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="bootstrapSource/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11" style="margin-top:10px;margin-bottom:10px;">
+                <a href="index.html"><img align="right" alt="中文版本" title="中文版本" src="./images/icon-chinese.png" style="margin-left:5%"></a>
+                <a href="/english/index.html"><img align="right" alt="English Version" title="English Version" src="./images/icon-english.png"></a>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle a" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav nav-justified">
+                        <li><a href="#">首页</a></li>
+                        <li><a href="#">关于我们</a></li>
+                        <li><a href="#">事件报道</a></li>
+                        <li><a href="#">孩子们</a></li>
+                        <li><a href="#">长期资助</a></li>
+                        <li><a href="#">联系与捐助</a></li>
+                        <li><a href="#">赞助者</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="row">
+            <img src="images/slogan.jpg" class="img-responsive center-block" alt="美中下一代基金会" />
+        </div>
+        <!--above are header-->
+        <!--<?php require_once('header.php');?>-->
